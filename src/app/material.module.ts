9d52c68d499d2c45d2f75a79c0836398db0f21f7
aretisd9 <aretisd>
2018-09-02 15:10:08 +0700

    create component

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
+import { MatBadgeModule } from '@angular/material';
+import { MatButtonModule } from '@angular/material';
 
 @NgModule({
-    imports: [],
-    exports: []
+    imports: [
+        MatBadgeModule,
+        MatButtonModule
+    ],
+    exports: [
+        MatBadgeModule,
+        MatButtonModule
+    ]
 })
 
 export class MaterialModule {}
